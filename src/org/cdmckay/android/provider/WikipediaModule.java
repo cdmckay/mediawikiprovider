d082ccf14d0de233eee81b3837f588543f017f9c
Cameron McKay <cdmckay@cdmckay.org>
2010-07-17 14:15:22 -0400

    Added copyright information.

--- src/org/cdmckay/android/provider/WikipediaModule.java
+++ src/org/cdmckay/android/provider/WikipediaModule.java
@@ -1,3 +1,18 @@
+/*******************************************************************************
+ * Copyright 2010 Cameron McKay
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ******************************************************************************/
 package org.cdmckay.android.provider;
 
 import java.io.ByteArrayOutputStream;
@@ -31,7 +46,7 @@ public class WikipediaModule implements Module {
 		MediaWikiMetaData.Search.URL
 	};
 	
-	// A temporary buffer used to hold the response of an http get request.
+	// A temporary buffer used to hold the response of an HTTP GET request.
 	private static byte[] sContentBuffer = new byte[512];
 	
 	
@@ -45,7 +60,7 @@ public class WikipediaModule implements Module {
 		final OpenSearchHandler handler = new OpenSearchHandler();
 		
 		try {			
-            Xml.parse(response, handler);
+			Xml.parse(response, handler);
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
